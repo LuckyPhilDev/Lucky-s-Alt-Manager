cf8c17860343d76585f709c59760fea55c19f0f9
Lucky Phil <jackclubs.dev@gmail.com>
2026-06-07 08:20:16 +0100

    Bump interface version to 120005

--- luckys_alt_toolkit/luckys_alt_toolkit.lua
+++ luckys_alt_toolkit/luckys_alt_toolkit.lua
@@ -149,7 +149,7 @@ eventFrame:SetScript("OnEvent", function(self, event, addonName)
             tt:AddLine(" ")
             tt:AddLine("Click: Open settings", 0.91, 0.86, 0.78)
             tt:AddLine("Middle-click: Toggle dev mode", 0.54, 0.49, 0.42)
-            tt:AddLine("Shift+drag: Move button", 0.54, 0.49, 0.42)
+            tt:AddLine("Drag: Move button", 0.54, 0.49, 0.42)
         end,
     })
 
